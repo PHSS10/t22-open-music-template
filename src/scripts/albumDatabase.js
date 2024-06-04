7388c2490a879b9350e4a2e9786ea77459632b7f
PHSS10 <pedrohenriquesousas14@gmail.com>
2024-06-03 22:29:43 -0300

    Create albumDatabase.js

---
+++ src/scripts/albumDatabase.js
@@ -0,0 +1,47 @@
+export const albumList = [
+    {
+      title: "Acabou o Chorare",
+      genre: "MPB",
+      band: "Novos Baianos",
+      price: "137.91",
+      img: "./src/assets/imgs/Baianos.jpg",
+    },
+    {
+      title: "The Wall",
+      genre: "Rock",
+      band: "Pink Floyd",
+      price: "123.56",
+      img: "./src/assets/imgs/Pink.jpg",
+    },
+    {
+      title: "To Pimp a Butterfly",
+      genre: "Rap",
+      band: "Kendrick Lamar",
+      price: "102.66",
+      img: "./src/assets/imgs/Lamar.jpg",
+    },
+    {
+      title: "Thriller",
+      genre: "Pop",
+      band: "Michael Jackson",
+      price: "142.32",
+      img: "./src/assets/imgs/MichaelJackson.jpg",
+    },
+    {
+      title: "All Eyez on Me",
+      genre: "Hip-Hop",
+      band: "2Pac",
+      price: "50.13",
+      img: "./src/assets/imgs/2pac.jpg",
+    },
+    {
+      title: "Dom de Sambar",
+      genre: "Samba",
+      band: "Turma do Pagode",
+      price: "51.67",
+      img: "./src/assets/imgs/turmaDoPagode.jpg",
+    },
+  ];
+
+
+
