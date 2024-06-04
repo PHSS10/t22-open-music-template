7b79f75d2cff82a85e94e02037c80b5523c7eb1f
PHSS10 <pedrohenriquesousas14@gmail.com>
2024-06-03 22:29:38 -0300

    Create inputRange.js

---
+++ src/scripts/inputRange.js
@@ -0,0 +1,10 @@
+export function applyInputRangeStyle() {
+    const inputRange = document.querySelector("#rangePreco");
+  
+    inputRange.addEventListener("input", (event) => {
+      const currentInputValue = event.target.value;
+      const runnableTrackProgress = (currentInputValue / inputRange.max) * 100;
+  
+      inputRange.style.background = `linear-gradient(to right,#6F00FF ${runnableTrackProgress}%, #DEE2E6 ${runnableTrackProgress}%)`;
+    });
+  }
